c6bf6d257e0876e75d32e0dbcb08e90f06de79b8
Istehsan <istehsanmirza@gmail.com>
2018-05-05 12:04:02 +0530

    magic ludo

---
+++ Ludo/JavaScript/Coin.js
@@ -0,0 +1,43 @@
+﻿jQuery(document).ready(function ($) {
+
+    //$('#coin').on('click', function () {
+    //    var flipResult = Math.random();
+    //    $('#coin').removeClass();
+    //    setTimeout(function () {
+    //        if (flipResult <= 0.5) {
+    //            $('#coin').addClass('heads');
+    //            console.log('it is head');
+    //        }
+    //        else {
+    //            $('#coin').addClass('tails');
+    //            console.log('it is tails');
+    //        }
+    //    }, 100);
+    //});
+
+    //ons.bootstrap()
+    //    .controller('NotificationController', function ($scope) {
+    //        $scope.alert = function (material) {
+    //            ons.notification.alert({
+    //                message: 'An error has occurred!',
+    //                modifier: material ? 'material' : undefined
+    //            });
+    //        }
+
+
+    //        $scope.prompt = function (material) {
+    //            var mod = material ? 'material' : undefined;
+    //            ons.notification.prompt({
+    //                message: "Please enter your age",
+    //                modifier: mod,
+    //                callback: function (age) {
+    //                    ons.notification.alert({
+    //                        message: 'You are ' + parseInt(age || 0) + ' years old.',
+    //                        modifier: mod
+    //                    });
+    //                }
+    //            });
+    //        }
+    //    });
+
+});
